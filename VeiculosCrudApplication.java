0c190c6d5da8aa171d1413e07a2fee340936bb95
TiagoCosta777 <contato7tiagocosta@gmail.com>
2025-06-17 10:11:55 -0300

    Create VeiculosCrudApplication.java

---
+++ VeiculosCrudApplication.java
@@ -0,0 +1,11 @@
+package com.veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeiculosCrudApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VeiculosCrudApplication.class, args);
+    }
+}
